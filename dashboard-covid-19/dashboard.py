d94998b9eecba7120053f6662e795bfc45b7f435
carlosrobertodevops <carlos.roberto.devops@gmail.com>
2024-06-21 22:18:35 -0300

    Ajustes

--- dashboard-covid-19/dashboard.py
+++ dashboard-covid-19/dashboard.py
@@ -1,12 +1,11 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output, ClientsideFunction
+from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
 import plotly.express as px
 import plotly.graph_objects as go
 
-import numpy as np
 import pandas as pd
 import json
 
@@ -37,12 +36,18 @@ select_columns = {
     "obitosAcumulado": "Óbitos Totais",
     "obitosNovos": "Óbitos por dia",
 }
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7bed242 (Ajustes)
 
 # =====================================================================
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.CYBORG])
 
+<<<<<<< HEAD
 
+=======
+>>>>>>> 7bed242 (Ajustes)
 fig = px.choropleth_mapbox(
     df_states_,
     locations="estado",
